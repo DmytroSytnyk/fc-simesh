adf04963b449b488ea2d4925e1366abf8611e9de
Dmytro Sytnyk <sytnikd@gmail.com>
2019-07-30 17:01:39 +0300

    Add configure_loc.m

---
+++ configure_loc.m
@@ -0,0 +1,4 @@
+% Automaticaly generated with fc_simesh.configure()
+oogmsh_dir=[pwd(),filesep,'..',filesep,'fc_oogmsh-0.0.17'];
+hypermesh_dir=[pwd(),filesep,'..',filesep,'fc_hypermesh-0.0.6'];
+siplt_dir=[pwd(),filesep,'..',filesep,'fc_siplt-0.0.2'];
